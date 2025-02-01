3cec27018cdacccf46ed43734bebaba307874195
chungchihhan <harryup2000@gmail.com>
2025-02-01 22:35:39 +0800

    refactor: add a condition to prevent flashing

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -3,19 +3,30 @@ import ImageGenerator from "@/components/ImageGenerator";
 
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Page() {
   const { data: session, status } = useSession();
   const router = useRouter();
+  const [checkingAuth, setCheckingAuth] = useState(true);
 
   useEffect(() => {
     if (status === "unauthenticated") {
       router.push("/login");
+    } else if (status !== "loading") {
+      setCheckingAuth(false);
     }
   }, [status, router]);
 
-  if (status === "loading") return <p>Loading...</p>;
+  if (checkingAuth) {
+    return (
+      <main className="min-h-screen w-full flex items-center justify-center">
+        <p className="text-xl text-white font-mono">
+          Checking authentication...
+        </p>
+      </main>
+    );
+  }
 
   return (
     <main className="min-h-screen w-full">
